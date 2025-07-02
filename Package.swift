b530c621016c16b8a258d99deed68258041e3e9f
Aden Eley <aden@kodelab.io>
2025-07-02 12:30:09 +0100

    update secp to match web3swift

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/Boilertalk/secp256k1.swift.git", from: "0.1.6"),
+        .package(url: "https://github.com/GigaBitcoin/secp256k1.swift", .upToNextMinor(from: "0.10.0")),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.5.1"),
         .package(url: "https://github.com/attaswift/BigInt.git", from: "5.0.0"),
         .package(url: "https://github.com/Flight-School/AnyCodable.git", from: "0.4.0"),
